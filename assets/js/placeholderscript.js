e5b0e9a52dc9ff66af51a1629e1db9d4e6980f1e
Conor Kelly <conorjkelly96@gmail.com>
2021-10-20 19:06:33 +0100

    pushing script adjustments to git

---
+++ assets/js/placeholderscript.js
@@ -0,0 +1,25 @@
+console.log("testing");
+
+const onLoad = function () {};
+
+window.addEventListener("load", onLoad);
+
+const onSubmit = function () {
+  // onSubmit
+};
+
+const initializeLocalStorage = function () {};
+
+const getDataFromLS = function () {};
+
+const validateCity = function (city) {};
+
+const renderCurrentData = function () {};
+
+const renderForecastData = function () {};
+
+const addCityToLocalStorage = function () {};
+
+const renderSearchHistory = function () {
+  // getDataFromLS();
+};
